9560fe6386b33075405ebbd782a5bc6dd9703fee
Dyst0rti0n <113265684+Dyst0rti0n@users.noreply.github.com>
2023-10-25 01:41:35 +0100

    webscrape1.py

--- webscrape1.py
+++ webscrape1.py
@@ -1,45 +1,40 @@
 import json
 import requests
-
 from bs4 import BeautifulSoup
 
-#URl for a seach query on github for 'useful python script' best I
-#could come up with
-URL = 'https://github.com/search?q=useful+python+script'
-
-data_to_save = []
-
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-
-}
-
-#response arg from URL
-response = requests.get(URL, headers=headers)
-
-def scrape_github():
-    #If statement to see reponse code 200
+def scrape_github(URL, headers):
+    data_to_save = []
+    response = requests.get(URL, headers=headers)
+    
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-    
-        # Github repositories are often listed under the class 'repo-list-item'
-        repos = soup.find_all('1i', class_='repo-list-item')
-    
+        
+        # Assuming for now that 'repo-list-item' is the correct class
+        repos = soup.find_all('li', class_='repo-list-item')
+        
         for repo in repos:
             repo_name = repo.find('a').get_text(strip=True)
             repo_link = 'https://github.com' + repo.find('a')['href']
             data_to_save.append({"Repository Name": repo_name, "Link": repo_link})
-           # print(f"Repository name: {repo_name}")
-           # print(f"Link: {repo_link}\n")#
-       
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
     
+    return data_to_save
+
+def save_to_json(data):
     with open('scraped_data.json', 'w') as file:
-        json.dump(data_to_save, file, indent=4)
-    
+        json.dump(data, file, indent=4)
+
 def main():
-    scrape_github()
-        
+    URL = 'https://github.com/search?q=useful+python+script'
+    
+    # Fixed the User-Agent header
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    
+    scraped_data = scrape_github(URL, headers)
+    save_to_json(scraped_data)
+
 if __name__ == "__main__":
     main()
